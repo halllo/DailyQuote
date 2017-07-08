e6239f510c8b4bbc78f73fe53ae44a6c112a4f6a
Manuel Naujoks <mnaujoks@gmail.com>
2017-07-08 16:48:00 +0200

    uwp version

---
+++ frontend_uwp/DailyQuote/Get.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DailyQuote
+{
+	public static class Get
+	{
+		public static async Task<Quote> CurrentQuote()
+		{
+			var httpClient = new HttpClient();
+			var brainyQuoteHtml = await httpClient.GetStringAsync("http://www.brainyquote.com/quotes_of_the_day.html?" + DateTime.Now.Ticks);
+			var quotesHtml = Regex.Matches(brainyQuoteHtml, @"<div class=""clearfix"">(.*?)</div>", RegexOptions.Singleline);
+			var quoteHtml = quotesHtml[0];
+
+			return new Quote(quoteHtml.Value);
+		}
+
+		public class Quote
+		{
+			public string Text { get; set; }
+			public string Author { get; set; }
+			
+			internal Quote(string html)
+			{
+				var matches = Regex.Matches(html, @"(?<=\>).*?(?=<)", RegexOptions.Multiline);
+				var notEmptyMatches = MatchCollectionExtensions.ToEnumerable(matches).Where(m => string.IsNullOrEmpty(m) == false).ToList();
+				
+				Text = UnEscape(string.Format("{0}", notEmptyMatches[0]));
+				Author = UnEscape(notEmptyMatches[1]);
+			}
+
+			private static string UnEscape(string str)
+			{
+				return str.Replace("&amp;", "&")
+						  .Replace("&gt;", ">")
+						  .Replace("&lt;", "<")
+						  .Replace("&quot;", "\"")
+						  .Replace("&apos;", "\'")
+						  .Replace("&#39;", "\'")
+						  ;
+			}
+		}
+
+		private static class MatchCollectionExtensions
+		{
+			public static IEnumerable<string> ToEnumerable(MatchCollection matches)
+			{
+				for (int i = 0; i < matches.Count; i++)
+				{
+					yield return matches[i].Value;
+				}
+			}
+		}
+	}
+}
---
+++ frontend_uwp/DailyQuote/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DailyQuote
+{
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+			this.Loaded += MainPage_Loaded;
+		}
+
+		private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+		{
+			await GetQuote();
+		}
+
+		private async void AppBarButton_RefreshClick(object sender, RoutedEventArgs e)
+		{
+			await GetQuote();
+		}
+
+		private async Task GetQuote()
+		{
+			this.quoteLoadingProgress.Visibility = Visibility.Visible;
+			try
+			{
+				var quote = await Get.CurrentQuote();
+				this.quoteTextBlock.Text = $"\"{quote.Text}\"\n\n{quote.Author}";
+			}
+			catch (Exception ex)
+			{
+				await Message(ex.Message);
+			}
+			this.quoteLoadingProgress.Visibility = Visibility.Collapsed;
+		}
+
+		public static async Task Message(string text)
+		{
+			await new MessageDialog(text).ShowAsync();
+		}
+	}
+}
